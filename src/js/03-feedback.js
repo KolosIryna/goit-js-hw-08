725b1f9647e5e78284f0e26b80656c06ff0d946a
KolosIryna <kolos.irina23@gmail.com>
2023-08-04 12:11:57 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,37 @@
 import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('.feedback-form input');
+const messageTextarea = document.querySelector('.feedback-form textarea');
+
 const LS_KEY = "feedback-form-state";
 
-localStorage.setItem(VCT_KE, JSON.stringify(name));
+formLocalStorage();
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInput() {
+    const formData = {
+      email: inputEmail.value,
+      message: messageTextarea.value,
+    };
+    localStorage.setItem(LS_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(LS_KEY)));
+    localStorage.removeItem(LS_KEY);
+    form.reset();
+     
+}
+
+function formLocalStorage() {
+    const localStorageValue = JSON.parse(localStorage.getItem(LS_KEY));
+    if (!localStorageValue) {
+      return;
+    }
+    inputEmail.value = localStorageValue.email;
+    messageTextarea.value = localStorageValue.message;
+}
